3d335ff606e18544cbfa757b32961d8e6e3c5ff1
Farber98 <juantuc98@gmail.com>
2022-09-21 20:42:32 -0300

    added channels and go routines to gain independence between producer and consumer.

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,39 +10,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweetCh chan<- *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			close(tweetCh) // As we receive channel ownership, must close it.
+			return
 		}
-
-		tweets = append(tweets, tweet)
+		tweetCh <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweets <-chan *Tweet, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for t := range tweets { //  Exits when channel closes
 		if t.IsTalkingAboutGo() {
-			fmt.Println(t.Username, "\ttweets about golang")
+			log.Println(t.Username, "\ttweets about golang")
 		} else {
-			fmt.Println(t.Username, "\tdoes not tweet about golang")
+			log.Println(t.Username, "\tdoes not tweet about golang")
 		}
 	}
 }
 
 func main() {
+	var wg sync.WaitGroup
+
 	start := time.Now()
 	stream := GetMockStream()
 
-	// Producer
-	tweets := producer(stream)
+	tweetCh := make(chan *Tweet)
 
-	// Consumer
-	consumer(tweets)
+	wg.Add(1)
+	go producer(stream, tweetCh, &wg) // One producer
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go consumer(tweetCh, &wg) // N independent consumers
+	}
+	wg.Wait() // Sync phase
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
